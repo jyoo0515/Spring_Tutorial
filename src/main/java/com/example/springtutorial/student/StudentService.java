5a648f40d1e86646756913d1624aed9542f6f2f9
jyoo0515 <jasonyoo0515@gmail.com>
2021-12-31 02:14:08 +0900

    init

---
+++ src/main/java/com/example/springtutorial/student/StudentService.java
@@ -0,0 +1,23 @@
+package com.example.springtutorial.student;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(
+                        1L,
+                        "John Doe",
+                        "john.doe@gmail.com",
+                        21,
+                        LocalDate.of(2000, Month.JANUARY, 4)
+                )
+        );
+    }
+}
